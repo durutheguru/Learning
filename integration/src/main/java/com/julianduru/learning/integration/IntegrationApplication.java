4a49b62c5f6c2a149c65b8c6309465a92c9a2da2
Julian Duru <durutheguru@gmail.com>
2023-05-06 00:30:01 +0100

    Code updates

--- integration/src/main/java/com/julianduru/learning/integration/IntegrationApplication.java
+++ integration/src/main/java/com/julianduru/learning/integration/IntegrationApplication.java
@@ -10,10 +10,12 @@ import org.springframework.integration.core.GenericTransformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.dsl.context.IntegrationFlowContext;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
@@ -26,35 +28,51 @@ public class IntegrationApplication {
     }
 
 
+    @Component
+    static class MyMessageSource implements MessageSource<String> {
+
+        @Override
+        public Message<String> receive() {
+            return MessageBuilder.withPayload(text()).build();
+        }
+
+    }
+
+
 	@Bean
 	MessageChannel greetings() {
 		return MessageChannels.direct().get();
 	}
 
 
-
     @Bean
-    ApplicationRunner runner() {
+    ApplicationRunner runner(IntegrationFlowContext context) {
         return args -> {
-            for (var i = 0; i < 10; i++)
-                greetings().send(
-                    MessageBuilder
-                        .withPayload(text())
-                        .build()
-                );
+//            for (var i = 0; i < 10; i++) {
+//                greetings().send(
+//                    MessageBuilder
+//                        .withPayload(text())
+//                        .build()
+//                );
+//                Thread.sleep(2000L);
+//            }
+            
         };
     }
 
 
-    private String text() {
+    private static String text() {
         return Math.random() > .5 ? "Hello World!! @ " + Instant.now() : "holla todo el mundo @ " + Instant.now();
     }
 
 
+
+
+
     @Bean
-    IntegrationFlow integrationFlow2() {
+    IntegrationFlow integrationFlow(MyMessageSource myMessageSource, int seconds, String filterText) {
         return IntegrationFlow
-            .from(greetings())
+            .from(myMessageSource, p -> p.poller(spec -> spec.fixedDelay(1000L)))
             .filter(String.class, source -> source.contains("holla"))
             .transform(
                 (GenericTransformer<String, String>) String::toUpperCase
