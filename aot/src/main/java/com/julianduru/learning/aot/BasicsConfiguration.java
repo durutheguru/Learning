1ce54f5681107b86388d7f5ce6d417f450249511
Julian Duru <durutheguru@gmail.com>
2023-05-08 17:00:06 +0100

    Code updates

---
+++ aot/src/main/java/com/julianduru/learning/aot/BasicsConfiguration.java
@@ -0,0 +1,33 @@
+package com.julianduru.learning.aot;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.stream.Stream;
+
+/**
+ * created by Julian Duru on 08/05/2023
+ */
+@Configuration
+class BasicsConfiguration {
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(CustomerRepository repository) {
+        return event -> repository
+            .saveAll(Stream.of("A", "B", "C").map(name -> new Customer(null, name)).toList())
+            .forEach(System.out::println);
+    }
+
+}
+
+
+record Customer(@Id Integer id, String name) {
+}
+
+
+interface CustomerRepository extends CrudRepository<Customer, Integer> {
+}
---
+++ aot/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+
+
+create table customer (
+   id   serial primary key,
+   name varchar(255) not null
+);
+
+
