2796aaf110a899a7b03bec11fa312d253ec6cbe4
Julian Duru <durutheguru@gmail.com>
2023-05-09 18:30:02 +0100

    Code updates

--- rabbit/src/main/java/com/julianduru/learning/rabbit/RabbitApplication.java
+++ rabbit/src/main/java/com/julianduru/learning/rabbit/RabbitApplication.java
@@ -1,13 +1,29 @@
 package com.julianduru.learning.rabbit;
 
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class RabbitApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(RabbitApplication.class, args);
+		Thread.currentThread().join();
 	}
+
+}
+
+
+@Configuration
+class RabbitConfiguration {
+
+
+	Exchange exchange() {
+		return ExchangeBuilder.directExchange()
+	}
+
 
 }
